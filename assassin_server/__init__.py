28f28753a19181769363f4040d7914352ef41740
Ellen Graham <grahamammal@gmail.com>
2019-03-11 22:19:00 -0500

    Updated data access

--- assassin_server/__init__.py
+++ assassin_server/__init__.py
@@ -1,15 +1,22 @@
 import os
+import redis
 
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 
+SESSION_TYPE = 'redis'
+sess = Session()
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
         DATABASE=os.path.join(app.instance_path, 'assassin_server.sqlite'),
+        SESSION_TYPE='filesystem'
     )
+
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -18,11 +25,16 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+
+
+    sess.init_app(app)
 
     # a simple page that says hello
     @app.route('/hello')
--- assassin_server/player_access.py
+++ assassin_server/player_access.py
@@ -9,70 +9,93 @@ from assassin_server.db import get_db
 
 bp = Blueprint('player_access', __name__, url_prefix='/player_access')
 
-@bp.route('/add_player')
+@bp.route('/add_player',  methods=['POST'])
 def add_player():
-    #doesn't check to make sure variables are valid
-    player_name =request.args.get('player_name', None)
-    game_code  = request.args.get('game_code', None)
+    content = request.get_json()
+
+    player_first_name=content['player_first_name']
+    player_last_name=content['player_last_name']
+    game_code=content['game_code']
 
     error=None
-
 
     db=get_db()
     if db.execute(
         'SELECT player_id FROM players'
-        ' WHERE player_name = ? AND game_code = ?',
-        (player_name, game_code)
+        ' WHERE player_first_name = ? AND player_last_name=? AND game_code = ?',
+        (player_first_name, player_last_name, game_code)
     ).fetchone() is not None:
-        error= "Player already exists"
+        error= 'Player already exists'
 
     if error is None:
         db.execute(
             'INSERT INTO players'
-            ' (player_name, game_code, is_alive, disputed_Got)'
-            ' VALUES (?, ?, 1, 0)',
-            (player_name, game_code)
+            ' (player_first_name, player_last_name, game_code, is_alive, disputed_Got)'
+            ' VALUES (?, ?, ?, 1, 0)',
+            (player_first_name, player_last_name, game_code)
         )
         db.commit()
 
-    return "Error: "+str(error)
+        session['this_player_id']=db.execute(
+            'SELECT player_id FROM players'
+            ' WHERE player_first_name = ? AND player_last_name=? AND game_code = ?',
+            (player_first_name, player_last_name, game_code)
+        ).fetchone()[0]
+
+    temp= db.execute(
+        'SELECT player_first_name FROM players'
+        ' WHERE game_code = ?',
+        (game_code,)
+    ).fetchone()
+
+    return str(error)
 
 #players can request data by providing their name and their game code
 @bp.route('/request_target')
 def request_target():
-    #doesn't check to make sure variables are valid
-    player_name  = request.args.get('player_name', None)
-    game_code  = request.args.get('game_code', None)
+
+    player_id=session['this_player_id']
+
+
 
     db=get_db()
-    target_name=db.execute(
-        'SELECT target_name FROM players'
-        ' WHERE player_name= ? AND game_code = ?',
-        (player_name, game_code)
+    target_name=temp= db.execute(
+        'SELECT target_first_name, target_last_name FROM players'
+        ' WHERE player_id = ?',
+        (player_id,)
     ).fetchone()
+
+
 
     if target_name is not None:
         target_map = {
-            "target_name": target_name[0]
+            'target_first_name': target_name[0],
+            'target_last_name': target_name[1]
         }
     else:
         target_map={
-            "target_name" : None
+            'target_first_name' : None,
+            'target_last_name' : None
+
         }
     return jsonify(target_map)
 
 # TODO: go to add player thing next?? or maybe return nothing and thats handle through the game
-@bp.route('/create_game')
+@bp.route('/create_game', methods=['POST'])
 def create_game():
-    game_name=request.args.get('game_name', None)
-    game_code=random.randint(1000, 10000)
+
+    content = request.get_json()
+
+    game_name=content['game_name']
+
+    game_code=random.randint(1000,10000)
 
     db=get_db()
     db.execute(
         'INSERT INTO games'
         ' (game_name, game_code, game_state)'
         ' (?, ?, 0)',
-        (game_name, game_state)
+        (game_name, game_code)
     )
     db.commit()
 
